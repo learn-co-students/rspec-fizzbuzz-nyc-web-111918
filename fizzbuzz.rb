13691bb529d1e20bececcdde689ac4188354647a
Matt Doering <mattdoering86@gmail.com>
2018-10-31 00:25:30 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-<<<<<<< HEAD
-=======
-
->>>>>>> bb527cf87fe597e204dda7a40f5a80b98e8204ec
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
     "FizzBuzz"
@@ -12,5 +8,6 @@ def fizzbuzz(number)
   elsif number % 5 == 0
     "Buzz"
   elsif number % 3 != 0 || number % 5 != 0
-    return nil
+    nil
+ end
 end
